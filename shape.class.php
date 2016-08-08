e95e854d1dbbafa2f50fcb4914ed8d581e7bafec
安诺爱成长 <1399487511@qq.com>
2016-08-08 18:43:43 +0800

    新建Shape类

--- shape.class.php
+++ shape.class.php
@@ -6,8 +6,26 @@
  * Date: 16/8/8
  * Time: 18:24
  */
-class Shape
+abstract class Shape
 {
+    public $shapeName;
+
+    abstract function area();
+
+    abstract function perimeter();
+
+    protected function validate($value, $message = '输入的值')
+    {
+        if ($value == "" || !is_numeric($value) || $value < 0) {
+            $message = $this->shapeName . $message;
+            echo '<font color="red">' . $message . '必须为非负的数字，并且不能为空</font><br>';
+            return false;
+        } else {
+            return true;
+        }
+
+
+    }
 
 
 }
