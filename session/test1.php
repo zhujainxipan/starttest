d458acb71fa7690ef108b320ef0bc5c427bae605
安诺爱成长 <1399487511@qq.com>
2016-08-15 14:37:01 +0800

    session的使用测试

---
+++ session/test1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: annuoaichengzhang
+ * Date: 16/8/15
+ * Time: 14:30
+ */
+
+session_start();
+
+$_SESSION['username'] = 'admin';
+
+echo 'session id'.session_id().'<br>';
+
+?>
+
+<a href="test2.php?<?php echo SID ?>">通过URL传递session id</a>
+
---
+++ session/test2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: annuoaichengzhang
+ * Date: 16/8/15
+ * Time: 14:33
+ */
+
+session_start();
+
+echo $_SESSION['username'] . '<br>';
+echo 'session id' . session_id() . '<br>';
+
+?>
